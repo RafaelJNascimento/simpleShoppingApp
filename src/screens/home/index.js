75e0fb6452661a3ec6e4045d7ea0ca228ce2d2b2
Rafael José do Nascimento <rafaeljosev10@gmail.com>
2020-08-14 02:32:21 -0300

    Home List

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,11 +1,51 @@
 import React, {Component} from 'react';
-import {Container, Form, Categories, ContentCategory, Text} from './styles';
+import {
+  Container,
+  Form,
+  Categories,
+  ContentCategory,
+  FormList,
+  ItemList,
+  ImageList,
+  Text,
+} from './styles';
 import Icon from 'react-native-vector-icons/Ionicons';
-import {StatusBar} from 'react-native';
+import {StatusBar, FlatList} from 'react-native';
+import {items} from '../../const';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Comprem os melhores Cavalos',
+    path: require('../../assets/img/cavalo.jpg'),
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Comprem as melhores Vacas',
+    path: require('../../assets/img/vaca.jpg'),
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f62',
+    title: 'Visite templos budistas',
+    path: require('../../assets/img/budismo.jpg'),
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f65',
+    title: 'Comprem os melhores Caprinos',
+    path: require('../../assets/img/caprino.jpg'),
+  },
+];
 
 export default class Home extends Component {
   componentDidMount() {}
   render() {
+    const Item = ({item}) => (
+      <ItemList>
+        <ImageList source={item.path} />
+        <Text style={{fontSize: 15}}>{item.title}</Text>
+      </ItemList>
+    );
+
     return (
       <Container>
         <StatusBar
@@ -15,74 +55,27 @@ export default class Home extends Component {
           backgroundColor="transparent"
         />
         <Form horizontal={true} showsHorizontalScrollIndicator={false}>
-          <Categories>
-            <ContentCategory>
-              <Icon
-                style={{alignSelf: 'center'}}
-                size={22}
-                color="#000"
-                name="flask-outline"
-              />
-              <Text>Laboratórios</Text>
-            </ContentCategory>
-          </Categories>
-          <Categories>
-            <ContentCategory>
-              <Icon
-                style={{alignSelf: 'center'}}
-                size={22}
-                color="#000"
-                name="cash-outline"
-              />
-              <Text>Banco</Text>
-            </ContentCategory>
-          </Categories>
-          <Categories>
-            <ContentCategory>
-              <Icon
-                style={{alignSelf: 'center'}}
-                size={22}
-                color="#000"
-                name="briefcase-outline"
-              />
-              <Text>Trabalho</Text>
-            </ContentCategory>
-          </Categories>
-          <Categories>
-            <ContentCategory>
-              <Icon
-                style={{alignSelf: 'center'}}
-                size={22}
-                color="#000"
-                name="bicycle-outline"
-              />
-              <Text>Lazer</Text>
-            </ContentCategory>
-          </Categories>
-          <Categories>
-            <ContentCategory>
-              <Icon
-                style={{alignSelf: 'center'}}
-                size={22}
-                color="#000"
-                name="fitness-outline"
-              />
-              <Text>Saúde</Text>
-            </ContentCategory>
-          </Categories>
-          <Categories>
-            <ContentCategory>
-              <Icon
-                style={{alignSelf: 'center'}}
-                size={22}
-                color="#000"
-                name="game-controller-outline"
-              />
-              <Text>Jogos</Text>
-            </ContentCategory>
-          </Categories>
+          {items.map((item, index) => (
+            <Categories key={index}>
+              <ContentCategory>
+                <Icon
+                  style={{alignSelf: 'center'}}
+                  size={22}
+                  color="#000"
+                  name={item.icon}
+                />
+                <Text>{item.name}</Text>
+              </ContentCategory>
+            </Categories>
+          ))}
         </Form>
-        <Container />
+        <FormList>
+          <FlatList
+            data={DATA}
+            renderItem={Item}
+            keyExtractor={(item, index) => String(index)}
+          />
+        </FormList>
       </Container>
     );
   }
--- src/screens/home/styles.js
+++ src/screens/home/styles.js
@@ -2,12 +2,38 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  background-color: #fff;
 `;
 
 export const Form = styled.ScrollView`
   max-height: 100px;
   width: 100%;
   background-color: #fff;
+`;
+
+export const FormList = styled.View`
+  flex: 1;
+  margin: 5px;
+  padding-top: 15px;
+  background-color: #fff;
+`;
+
+export const ItemList = styled.View`
+  max-height: 170px;
+  min-height: 170px;
+  width: 100%;
+  padding-top: 10px;
+  margin-vertical: 8px;
+  background-color: #eee;
+  border-radius: 7px;
+  align-items: center;
+`;
+
+export const ImageList = styled.Image`
+  height: 130px;
+  border-radius: 7px;
+  width: 95%;
+  margin-bottom: 3px;
 `;
 
 export const Categories = styled.TouchableOpacity`
