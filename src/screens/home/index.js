bcb9de397e01438e892fdcff3264f151b26f034f
Rafael José do Nascimento <rafaeljosev10@gmail.com>
2020-08-14 00:23:44 -0300

    static category list

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,13 +1,81 @@
 import React, {Component} from 'react';
-import {Container, Text} from './styles';
+import {Container, Form, Categories, ContentCategory, Text} from './styles';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class Home extends Component {
   componentDidMount() {}
-
   render() {
     return (
       <Container>
-        <Text>Home</Text>
+        <Form horizontal={true} showsHorizontalScrollIndicator={false}>
+          <Categories>
+            <ContentCategory>
+              <Icon
+                style={{alignSelf: 'center'}}
+                size={22}
+                color="#000"
+                name="flask-outline"
+              />
+              <Text>Laboratórios</Text>
+            </ContentCategory>
+          </Categories>
+          <Categories>
+            <ContentCategory>
+              <Icon
+                style={{alignSelf: 'center'}}
+                size={22}
+                color="#000"
+                name="cash-outline"
+              />
+              <Text>Banco</Text>
+            </ContentCategory>
+          </Categories>
+          <Categories>
+            <ContentCategory>
+              <Icon
+                style={{alignSelf: 'center'}}
+                size={22}
+                color="#000"
+                name="briefcase-outline"
+              />
+              <Text>Trabalho</Text>
+            </ContentCategory>
+          </Categories>
+          <Categories>
+            <ContentCategory>
+              <Icon
+                style={{alignSelf: 'center'}}
+                size={22}
+                color="#000"
+                name="bicycle-outline"
+              />
+              <Text>Lazer</Text>
+            </ContentCategory>
+          </Categories>
+          <Categories>
+            <ContentCategory>
+              <Icon
+                style={{alignSelf: 'center'}}
+                size={22}
+                color="#000"
+                name="fitness-outline"
+              />
+              <Text>Saúde</Text>
+            </ContentCategory>
+          </Categories>
+          <Categories>
+            <ContentCategory>
+              <Icon
+                style={{alignSelf: 'center'}}
+                size={22}
+                color="#000"
+                name="game-controller-outline"
+              />
+              <Text>Jogos</Text>
+            </ContentCategory>
+          </Categories>
+        </Form>
+        <Container />
       </Container>
     );
   }
--- src/screens/home/styles.js
+++ src/screens/home/styles.js
@@ -4,4 +4,37 @@ export const Container = styled.View`
   flex: 1;
 `;
 
-export const Text = styled.Text``;
+export const Form = styled.ScrollView`
+  max-height: 100px;
+  width: 100%;
+  background-color: #fff;
+`;
+
+export const Categories = styled.TouchableOpacity`
+  align-self: center;
+  min-height: 80px;
+  min-width: 80px;
+  max-height: 80px;
+  max-width: 80px;
+  margin: 5px;
+  border-radius: 50px;
+  background: blue;
+`;
+
+export const ContentCategory = styled.View`
+  border-radius: 50px;
+  min-height: 70px;
+  min-width: 70px;
+  max-height: 70px;
+  max-width: 70px;
+  margin-top: 5px;
+  padding-top: 15px;
+  align-self: center;
+  align-content: center;
+  background: #eee;
+`;
+
+export const Text = styled.Text`
+  font-size: 10px;
+  align-self: center;
+`;
